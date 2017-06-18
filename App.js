da9c83600af9ad3a905bcd2a2279c4ff7abdaed3
Myles O'Connor <hello@myles.la>
2017-06-18 15:01:31 -0700

    Added ESlint, project directory structure

--- App.js
+++ App.js
@@ -1,29 +1,3 @@
-import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import App from './app/index';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <StatusBar
-            barStyle="light-content"
-        />
-        <Text style={styles.text}>Open up App.js to start working on your app!</Text>
-        <Text style={styles.text}>Changes you make will automatically reload.</Text>
-        <Text style={styles.text}>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#aaaaaa',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  text: {
-    color: '#ffffff'
-  }
-});
+export default App;
