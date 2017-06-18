66a48255f8e5dc384eefaf9c168870fa8ebcc067
Myles O'Connor <hello@myles.la>
2017-06-18 14:32:53 -0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar
+            barStyle="light-content"
+        />
+        <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+        <Text style={styles.text}>Changes you make will automatically reload.</Text>
+        <Text style={styles.text}>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#aaaaaa',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    color: '#ffffff'
+  }
+});
