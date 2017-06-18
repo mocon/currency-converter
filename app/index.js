110655d1fba49aa70e36b24aa51cb14889370e9f
Myles O'Connor <hello@myles.la>
2017-06-18 15:22:17 -0700

    Fixed ESlint

--- app/index.js
+++ app/index.js
@@ -4,11 +4,11 @@ import { StyleSheet, Text, View, StatusBar } from 'react-native';
 export default class App extends React.Component {
     render() {
         return (
-            <View style={styles.container}>
+            <View style={ styles.container }>
                 <StatusBar barStyle="light-content" />
-                <Text style={styles.text}>Open up App.js to start working on your app!</Text>
-                <Text style={styles.text}>Changes you make will automatically reload.</Text>
-                <Text style={styles.text}>Shake your phone to open the developer menu.</Text>
+                <Text style={ styles.text }>Open up App.js to start working on your app!</Text>
+                <Text style={ styles.text }>Changes you make will automatically reload.</Text>
+                <Text style={ styles.text }>Shake your phone to open the developer menu.</Text>
             </View>
         );
     }
