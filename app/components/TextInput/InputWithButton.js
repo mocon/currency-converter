0c05cc4951493e2bf75461f668875b1ca225752c
Myles O'Connor <hello@myles.la>
2017-06-24 17:31:00 -0700

    Styled <InputWithButton />

---
+++ app/components/TextInput/InputWithButton.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { View, Text, TouchableHighlight, TextInput } from 'react-native';
+import PropTypes from 'prop-types';
+import color from 'color';
+
+import styles from './styles';
+
+const InputWithButton = (props) => {
+    const { buttonText, onPress, editable = true } = props;
+
+    const underlayColor = color(styles.$buttonBackgroundColorBase).darken(styles.$buttonBackgroundColorModifier);
+
+    const containerStyles = [ styles.container ];
+    if (!editable) containerStyles.push(styles.containerDisabled);
+
+    return (
+        <View style={ containerStyles }>
+            <TouchableHighlight
+                underlayColor={ underlayColor }
+                style={ styles.buttonContainer }
+                onPress={ onPress }
+            >
+                <Text style={ styles.buttonText }>{ buttonText }</Text>
+            </TouchableHighlight>
+            <View style={ styles.border } />
+            <TextInput
+                style={ styles.input }
+                underlineColorAndroid="transparent"
+                { ...props }
+            />
+        </View>
+    );
+};
+
+InputWithButton.propTypes = {
+    buttonText: PropTypes.string.isRequired,
+    onPress: PropTypes.func.isRequired,
+    editable: PropTypes.bool
+};
+
+export default InputWithButton;
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StatusBar, View, Text } from 'react-native';
 
 import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
+import { InputWithButton } from '../components/TextInput';
 
-const Home = () => (
-    <Container>
-        <StatusBar translucent={ false } barStyle="light-content" />
-        <Logo />
-    </Container>
-);
+const TEMP_BASE_CURRENCY = 'USD';
+const TEMP_QUOTE_CURRENCY = 'GBP';
+const TEMP_BASE_PRICE = '100';
+const TEMP_QUOTE_PRICE = '79.74';
+
+class Home extends Component {
+    state = {
+        name: 'Myles'
+    };
+
+    handlePressBaseCurrency = () => {
+        console.log('Pressed base');
+    }
+
+    handlePressQuoteCurrency = () => {
+        console.log('Pressed quote');
+    }
+
+    handleTextChange = (text) => {
+        console.log('Changed text to', text);
+    }
+
+    render() {
+        return (
+            <Container>
+                <StatusBar translucent={ false } barStyle="light-content" />
+                <Logo />
+                <InputWithButton
+                    buttonText={ TEMP_BASE_CURRENCY }
+                    onPress={ this.handlePressBaseCurrency }
+                    defaultValue={ TEMP_BASE_PRICE }
+                    keyboardType="numeric"
+                    onChangeText={ this.handleTextChange }
+                />
+                <InputWithButton
+                    buttonText={ TEMP_QUOTE_CURRENCY }
+                    onPress={ this.handlePressQuoteCurrency }
+                    value={ TEMP_QUOTE_PRICE }
+                    editable={ false }
+                />
+            </Container>
+        );
+    }
+}
 
 export default Home;
